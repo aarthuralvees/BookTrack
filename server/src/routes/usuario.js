9d7ad9043aa20c3e433965af58ec4e91cff10b32
Arthur <aarthuralvees@gmail.com>
2025-05-12 22:29:25 -0300

    fix(auth error)

--- server/src/routes/usuario.js
+++ server/src/routes/usuario.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { UserController } from '../controllers/index.js';
+import verifyToken from "../middleware/auth.js"
 
 const userRouter = Router();
 
 userRouter.post('/create', UserController.createUser)                //criar user
-userRouter.get('/userId/:id', UserController.findUserById);          //info de um usuário
-userRouter.get('/userEmail/:email', UserController.findUserByEmail); //info de um usuário
+userRouter.get('/userId/:id', verifyToken, UserController.findUserById);          //info de um usuário
+userRouter.get('/userEmail/:email', verifyToken, UserController.findUserByEmail); //info de um usuário
 userRouter.get('/allUsers', UserController.listUsers);               //info de vários
-userRouter.put('/update/:id', UserController.updateUser);            //atualizar
-userRouter.delete('/delete/:id', UserController.deleteUser);         //deletar
+userRouter.put('/update/:id', verifyToken, UserController.updateUser);            //atualizar
+userRouter.delete('/delete/:id', verifyToken, UserController.deleteUser);         //deletar
 
 export default userRouter;
